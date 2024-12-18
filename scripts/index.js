e17ff3fd0cf72c5f5f73d42b7313aea53a3feccd
fbslo <admin@fbslo.net>
2024-12-18 14:36:37 +0100

    update baseTest

--- scripts/index.js
+++ scripts/index.js
@@ -8,16 +8,18 @@ const { verify } = require("./utils/verify")
 main()
 
 async function main(){
+    // const [deployer, admin] = await hre.ethers.getSigners();
+
     // console.log(await deployCoreScript.main())
 
     // const oracleConfig = {
     //     baseToken: "0xe0bdd7e8b7bf5b15dcDA6103FCbBA82a460ae2C7", //asset WETH
-    //     quoteToken: "0xe2FbC9cB335A65201FcDE55323aE0F4E8A96A616", //collateral stHYPE
+    //     quoteToken: "0xe2FbC9cB335A65201FcDE55323aE0F4E8A96A616", //collateral
     //     chainlinkMultiplyAddress: '0xdF6CeC37cF3EB6c69146A0787f8488B94ef3c22E', //WETH provider
-    //     chainlinkDivideAddress: '0x713c630Cbb3A37b45bA3921125abb3D0D4F4d8ef', //stHYPE provider
+    //     chainlinkDivideAddress: '0x11CE4F650957e03f5a465839F7f699d19dB43c41', //collateral provider
     //     maxOracleDelay: "1000000",
     //     timelockAddress: "0x33e99304C3F628067Bb0939b21820d7Ba39913AB",
-    //     name: "ChainlinkSingle-WETH-stTESTH"
+    //     name: "ChainlinkSingle-WETH-stHYPE"
     // }
     // let { oracle } = await deployOracleScript.main(oracleConfig)
     // console.log(oracle)
@@ -38,18 +40,18 @@ async function main(){
     // let interestRate = await deployInterestRateScript.main(interestRateConfig)
     // console.log(interestRate)
 
-    let pairConfig = {
-        hyperlendPairRegistry: "0x274396Ec36D17dAbC018d9437D5a4C0D0fD503D0",
-        hyperlendPairDeployerAddress: "0x0d17A8856aD18C5CCE4B426744Fee0d918c6a7D4",
-        assetTokenAddress: "0xe0bdd7e8b7bf5b15dcDA6103FCbBA82a460ae2C7", //borrow WETH
-        collateralTokenAddress: "0xe2FbC9cB335A65201FcDE55323aE0F4E8A96A616", //supply stHYPE as collateral
-        interestRateAddress: '0x339a57F0bc7ad67dC1956aE5254038AEae1cAe9B',
-        oracleAddress: '0x9d59b30B5311B31F8EEE7dB8252355B860DCd2C1',
-        maxOracleDeviation: "5000", //5%
-        fullUtilizationRate: "9500000000", //~30% start APR
-        maxLTV: "75000", //75%
-        cleanLiquidationFee: "10000", //10%
-        protocolLiquidationFee: "10000" //10% of the liquidator's fee
-    }
-    console.log(await deployPairScript.main(pairConfig))
+    // let pairConfig = {
+    //     hyperlendPairRegistry: "0x274396Ec36D17dAbC018d9437D5a4C0D0fD503D0",
+    //     hyperlendPairDeployerAddress: "0x0d17A8856aD18C5CCE4B426744Fee0d918c6a7D4",
+    //     assetTokenAddress: "0xe0bdd7e8b7bf5b15dcDA6103FCbBA82a460ae2C7", //borrow WETH
+    //     collateralTokenAddress: "0x453b63484b11bbF0b61fC7E854f8DAC7bdE7d458", //supply MBTC as collateral
+    //     interestRateAddress: '0x339a57F0bc7ad67dC1956aE5254038AEae1cAe9B',
+    //     oracleAddress: '0x58B5039fF60227db6dFf6186E57340fc7a853F84',
+    //     maxOracleDeviation: "5000", //5%
+    //     fullUtilizationRate: "9500000000", //~30% start APR
+    //     maxLTV: "75000", //75%
+    //     cleanLiquidationFee: "10000", //10%
+    //     protocolLiquidationFee: "10000" //10% of the liquidator's fee
+    // }
+    // console.log(await deployPairScript.main(pairConfig))
 }
